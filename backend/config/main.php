26ffebe744d9d24d5cd4acc5da651c286eb6c046
RUSya82 <mail-spam2012@yandex.ru>
2019-05-15 19:12:54 +0500

    admin lte

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,6 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
+    'layout' => 'admin-lte/main',
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
